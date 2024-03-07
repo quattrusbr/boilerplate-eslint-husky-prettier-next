3e610edf2e722f11b9ecf739df39c15017ebaae9
ismaelrodino <ismael.rodino@quattrus.com>
2024-03-07 12:04:58 -0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,12 @@ import '@/styles/globals.css';
 
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { ReactNode } from 'react';
+import { ReactNode } from 'react'
 
-import { MainProvider } from '@/components/providers/MainProvider';
-import { MainLayout } from '@/components/templates/MainLayout';
+import { MainProvider } from '@/components/providers/MainProvider'
+import { MainLayout } from '@/components/templates/MainLayout'
 
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-primary' });
 
